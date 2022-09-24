435620484a2d2333ea33c44ae2b6cf54114c6e22
ochom <ochomrichard752@gmail.com>
2022-09-24 11:49:46 +0300

    update: tag.sh

--- tag.sh
+++ tag.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-git tag -a v$1 -m "Version $1" && git push origin --tags
+# This script will tag the current commit with the version number
+# and push the tag to the remote repository.
+# usage: gtag [tagname]
+
+# To activate this script, run ./tag.sh from the command line.
+# then you can run gtag from anywhere in the repository.
+
+alias gtag='function _gtag(){ git tag -a v$1 -m "Version $1"; git push origin --tags; };_gtag'
