049046cd104e4b507de6eeedb13bd4b0c36c5eb3
ochom <ochomrichard752@gmail.com>
2022-09-24 12:46:52 +0300

    test: organization repository

--- models/accounts.go
+++ models/accounts.go
@@ -1,21 +1,33 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 // Organization ...
 type Organization struct {
-	BaseModel
-	Name    string `json:"name"`
-	Address string `json:"address"`
-	Mobile  string `json:"mobile"`
-	Email   string `json:"email"`
+	ID        string         `json:"id" gorm:"primaryKey"`
+	Name      string         `json:"name"`
+	Address   string         `json:"address"`
+	Mobile    string         `json:"mobile"`
+	Email     string         `json:"email"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt"`
 }
 
 // User ...
 type User struct {
-	BaseModel
-	FirstName      string `json:"firstName"`
-	LastName       string `json:"lastName"`
-	Mobile         string `json:"mobile"`
-	Email          string `json:"email"`
-	Password       string `json:"password"`
-	OrganizationID string `json:"organizationID"`
+	ID             string         `json:"id" gorm:"primaryKey"`
+	FirstName      string         `json:"firstName"`
+	LastName       string         `json:"lastName"`
+	Mobile         string         `json:"mobile"`
+	Email          string         `json:"email"`
+	Password       string         `json:"password"`
+	OrganizationID string         `json:"organizationID"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	UpdatedAt      time.Time      `json:"updatedAt"`
+	DeletedAt      gorm.DeletedAt `json:"deletedAt"`
 }
--- models/services.go
+++ models/services.go
@@ -1,21 +1,33 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 // Service ...
 type Service struct {
-	BaseModel
-	Name           string `json:"name"`
-	OfferName      string `json:"offerName"`
-	OfferCode      string `json:"offerCode"`
-	Description    string `json:"description"`
-	OrganizationID string `json:"organizationID"`
+	ID             string         `json:"id" gorm:"primaryKey"`
+	Name           string         `json:"name"`
+	OfferName      string         `json:"offerName"`
+	OfferCode      string         `json:"offerCode"`
+	Description    string         `json:"description"`
+	OrganizationID string         `json:"organizationID"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	UpdatedAt      time.Time      `json:"updatedAt"`
+	DeletedAt      gorm.DeletedAt `json:"deletedAt"`
 }
 
 // Subscriber ...
 type Subscriber struct {
-	BaseModel
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Mobile    string `json:"mobile"`
-	Email     string `json:"email"`
-	ServiceID string `json:"organizationID"`
+	ID        string         `json:"id" gorm:"primaryKey"`
+	FirstName string         `json:"firstName"`
+	LastName  string         `json:"lastName"`
+	Mobile    string         `json:"mobile"`
+	Email     string         `json:"email"`
+	ServiceID string         `json:"organizationID"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt"`
 }
