2840100d0182d2dd7ca5508b6d662809bed3f48b
ochom <ochomrichard752@gmail.com>
2022-09-24 14:33:54 +0300

    feat: auth response

--- dto/output.go
+++ dto/output.go
@@ -1,1 +1,11 @@
 package dto
+
+import "github.com/ochom/sdp-lib/models"
+
+// AuthResponse ...
+type AuthResponse struct {
+	User         models.User         `json:"user"`
+	Organization models.Organization `json:"organization"`
+	AccessToken  string              `json:"accessToken"`
+	RefreshToken string              `json:"refreshToken"`
+}
