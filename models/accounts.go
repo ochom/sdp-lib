870f1795ea145adcbadfe3016b8405bc2446fde2
ochom <ochomrichard752@gmail.com>
2022-09-29 12:37:15 +0300

    add: model fields

--- models/accounts.go
+++ models/accounts.go
@@ -8,16 +8,17 @@ import (
 
 // Organization ...
 type Organization struct {
-	ID         string         `json:"id" gorm:"primaryKey"`
-	Name       string         `json:"name"`
-	Address    string         `json:"address"`
-	Mobile     string         `json:"mobile"`
-	Email      string         `json:"email"`
-	IsVerified bool           `json:"isVerified"`
-	IsActive   bool           `json:"isActive"`
-	CreatedAt  time.Time      `json:"createdAt"`
-	UpdatedAt  time.Time      `json:"updatedAt"`
-	DeletedAt  gorm.DeletedAt `json:"deletedAt"`
+	ID            string         `json:"id" gorm:"primaryKey"`
+	Name          string         `json:"name"`
+	Address       string         `json:"address"`
+	Mobile        string         `json:"mobile"`
+	Email         string         `json:"email"`
+	ContactPerson string         `json:"contactPerson"` // Contact person name
+	IsVerified    bool           `json:"isVerified"`
+	IsActive      bool           `json:"isActive"`
+	CreatedAt     time.Time      `json:"createdAt"`
+	UpdatedAt     time.Time      `json:"updatedAt"`
+	DeletedAt     gorm.DeletedAt `json:"deletedAt"`
 }
 
 // User ...
